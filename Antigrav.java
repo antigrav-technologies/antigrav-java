e256ababc6585a2dfd7835e8ab55e73483706e02
cake64 <166376575+cake64@users.noreply.github.com>
2024-09-12 13:11:50 +0300

    Update Antigrav.java
    public static <T> T decode(String s) throws говно {
        throw new говно();
    }

--- Antigrav.java
+++ Antigrav.java
@@ -1,10 +1,41 @@
 package tema5002.github.io;
 
-public class Antigrav {
-    public static String dumpToString(Object obj) {
-        return "not implemented";
+class говно extends Exception {}
+
+class Decoder {
+    public static <T> T decode(String s) throws говно {
+        throw new говно();
     }
-    public static <T> T loadFromString(String s) {
-        throw new NullPointerException();
+}
+
+class Encoder {
+    public static String encode(
+        Object o,
+        boolean sortKeys,
+        int indent,
+        boolean ensureASCII,
+        boolean allowNaN,
+        boolean forceSave
+    ) {
+        return "throw new говно();";
+    }
+}
+
+public class Antigrav {
+    public static String dumpToString(
+        Object o,
+        boolean sortKeys,
+        int indent,
+        boolean ensureASCII,
+        boolean allowNaN,
+        boolean forceSave
+    ) {
+        return Encoder.encode(o, sortKeys, indent, ensureASCII, allowNaN, forceSave);
+    }
+    public static String dumpToString(Object o) {
+        return dumpToString(o, false, -1, true, false, false);
+    }
+    public static <T> T loadFromString(String s) throws говно {
+        return Decoder.decode(s);
     }
 }
