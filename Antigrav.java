226b9260869b195d13ea230af967e680957e0346
cake64 <166376575+cake64@users.noreply.github.com>
2024-09-12 12:59:56 +0300

    Add files via upload

---
+++ Antigrav.java
@@ -0,0 +1,10 @@
+package tema5002.github.io;
+
+public class Antigrav {
+    public static String dumpToString(Object obj) {
+        return "not implemented";
+    }
+    public static <T> T loadFromString(String s) {
+        throw new NullPointerException();
+    }
+}
